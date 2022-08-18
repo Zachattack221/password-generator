4a4fb4ad07c5ca65669b758c32bf16e555a77b21
Zachattack221 <zsvd09@gmail.com>
2022-08-18 19:01:10 -0400

    Added variables corresponding to possible inputs for each character type, notated in JS file

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Setting variables that correspond to the possible inputs for each character type: uppercase, lowercase, bumeric, or special
+
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var numOptions = "0123456789";
+var specialChar = "\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+ return;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
